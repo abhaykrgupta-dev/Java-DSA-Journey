3a70245c875162542c9fc1f402867cf90003a2e8
Abhay Kumar Gupta <ayushrajgpt28@gmail.com>
2026-04-19 15:51:28 +0530

    Day 1: Added Hello World

---
+++ src/Day01HelloWorld.java
@@ -0,0 +1,7 @@
+public class Day01HelloWorld{
+    public static void main( String [] args){
+        System.out.println(" Hello World ! ");
+        //My First Project Is Completed !
+        //Shortcut for commit is "ctrl + K"
+    }
+}
