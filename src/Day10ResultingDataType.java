9cb6cd59bcb8addb41fedace79175be2cd9e3f24
Abhay Kumar Gupta <ayushrajgpt28@gmail.com>
2026-05-23 09:06:12 +0530

    Day 13: added the concept of casting and its type

--- src/Day10ResultingDataType.java
+++ src/Day10ResultingDataType.java
@@ -19,23 +19,25 @@ public class Day10ResultingDataType {
         char c ='@';
         double d = 89556.6598;
         float f = 199.965F;
+        float F = 100.998F;
         long l = 56489652565L;
         short s = 55;
 
 
         int ans1 = b+s;              // any other datatype except int will give error if you try to run
         int ans2 = s+a;              // any other datatype except int will give error if you try to run
-        float ans3 = l+f;            // any other datatype except int will give error if you try to run
-        float ans4 = a+f;            // any other datatype except int will give error if you try to run
+        float ans3 = l+f;            // any other datatype except float will give error if you try to run
+        float ans4 = a+f;            // any other datatype except float will give error if you try to run
         int ans5 = c+a;              // any other datatype except int will give error if you try to run
         int ans6 = c+s;              // any other datatype except int will give error if you try to run
-        double ans7 = l+d;           // any other datatype except int will give error if you try to run
-        double ans8 = f+d;           // any other datatype except int will give error if you try to run
+        double ans7 = l+d;           // any other datatype except double will give error if you try to run
+        double ans8 = f+d;           // any other datatype except double will give error if you try to run
 
         // if you want you can try running program by changing datatype without changing variables it will definitely show error
         // For Eg :- Try running int ans7 = l+d; will give error,
-        // but notice that if you change datatype of int to float or double then it will work smoothly without giving error , its because float and double can print integers as well as decimals
+        // but notice that if you change datatype of int to float or double then it will work smoothly without giving error , its because float and double can print integers as well as decimals and this is known as implicit casting or widening primitive conversion
         // but if you exchange float with int data types you will get error as int cant hold decimals it can only store integers and thats basic logic
+        // Casting:- we can store small containers material into a big container but if we try to store big containers material into the small one it will over flow and we will loose some data
 
 //        int wrongans7 = l+d;                 this will show error
 //        System.out.println(wrongans7);
@@ -47,6 +49,23 @@ public class Day10ResultingDataType {
         System.out.println(ans6);
         System.out.println(ans7);
         System.out.println(ans8);
+
+//        Eg to understand casting better
+
+        double price = 100.00;
+        double FinalPrice = price + 18;
+        System.out.println(FinalPrice);
+
+        int price_ = 100;
+//        int finalprice = price + 18.99;         //   here , you will face an error here as java refuses to do implicit casting here , java prevents automatic "narrowing" to protect our data from being lost.
+
+
+//        Now if we want to store it and we are redy to loose some of our data then we can do this as following:-
+
+        int finalPrice = price_ + (int)18.99;
+        System.out.println( finalPrice);      // you can observe that we lost our value that was written after the decimal point i.e 0.99
+                                              // but you will find out there is no error and this is called explicit casting
+
 
 
     }
