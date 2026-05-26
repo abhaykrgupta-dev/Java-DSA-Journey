4d0b4e9bdc953279b4bb3d9230d0fd9316d8bb0b
Abhay Kumar Gupta <ayushrajgpt28@gmail.com>
2026-05-26 11:07:42 +0530

    Day 7: Types Of Operators

--- src/Day07TypesOfOperators.java
+++ src/Day07TypesOfOperators.java
@@ -22,14 +22,6 @@ public class Day07TypesOfOperators{
         System.out.println(e);
         System.out.println(f);
 
-//        Assignment Operators [ = , += . *= ]
-        int ab = 125;  // Here = works as an assignment Operator " = assigns 125 to variable ab "
-        int p = 8;
-        p += 102;    //Here += works as an assignment operator as it will print p+102 which is 8+102=110
-    //   p *= 12;     // it simply means p*12 which is 8*12=96
-
-        System.out.println(p);
-
         // Unary OPERATORS
         // these operators usually requires only one value to be declared
         int numb = 105;
@@ -40,22 +32,39 @@ public class Day07TypesOfOperators{
         int num = 128;
         System.out.println(num++);   // [129]  // by this we instructed java to first print num and after that add 1 and store it
         System.out.println(num);     // [129]  // now if we try to print num we already know that previously num was enhanced by using ++ so we will get the enhanced output
-                                               // simply remember that here we instructed to first print the num then enhanced is as we wrote ++ after num
+        // simply remember that here we instructed to first print the num then enhanced is as we wrote ++ after num
         int number = 92;
         System.out.println(++number);  // [92] // by using ++ before numb we are instructing java that first enhance the number by 1 and then print it
         System.out.println(number);    // [93] // now if we print this we will figure out we will get output as 93 as it was already enhanced previously by suing ++number
-                                               // simply remember that here we instructed java to first enhance the number and print it
+        // simply remember that here we instructed java to first enhance the number and print it
 
 //        Similarly we use num-- and --numb for decrement
 
-//      Comparison Operators [ ==,<,>,<=,>= ]
+
+
+//        Assignment Operators [ = , += . *= ]
+        int ab = 125;  // Here = works as an assignment Operator " = assigns 125 to variable ab "
+        int p = 8;
+        p += 102;    //Here += works as an assignment operator as it will print p+102 which is 8+102=110
+    //   p *= 12;     // it simply means p*12 which is 8*12=96
+
+        System.out.println(p);
+
+
+
+//      Comparison Operators [ ==,!=,<,>,<=,>= ]
         System.out.println(" is 28 = 40? "+(a==b));      // the == gives only true or false as a result and is used for comparison
         System.out.println("is 128 = 128? " +(128==128));
         System.out.println(" is 1999 greater than 9995? "+(1999>9995));
         System.out.println(" is 2589 less than 8567 ? "+(2589<8567));
+        System.out.println(" is 2658 not equal to 1256 ? " + (2658!=1256));    // here,!= represent not equal to
+        System.out.println(" is 158 not equal to 158 ? " + (158!=158));
+        System.out.println(" is 50 greater than equal to 125 ? " + (50>=125));
+        System.out.println(" is -12 less than equal to -8 ? " + (-12 <= -8));
         // Similarly we use <= and >=
 
-//        Logical Operators [ && , || , ! ]
+
+//      Logical Operators [ && , || , ! ]
 
     // The and method "&&"
         // it specifies both condition need to be true to get the output as true
@@ -70,6 +79,7 @@ public class Day07TypesOfOperators{
         System.out.println( 68>56 || 109>996);
         System.out.println(168<999 || 56 <123);
         System.out.println( 25698<1258 || 569885<4569);
+
 
 //      Bitwise Operators [ & , | ]
         // this is not necessary at the moment
