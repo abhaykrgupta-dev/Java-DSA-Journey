fa59b8461fafad62de369d88e1bd872793a1d23f
Abhay Kumar Gupta <ayushrajgpt28@gmail.com>
2026-05-28 14:14:31 +0530

    Day 16: IF Else Statements

--- src/Day16ConditionalOperators.java
+++ src/Day16ConditionalOperators.java
@@ -26,14 +26,72 @@ public class Day16ConditionalOperators {
 
         int remainder = num%2;
 
-        if (remainder==0){
+        if (remainder==0)
             System.out.println("Even Number");
+        else
+            System.out.println("Odd Number");
+//       while using if else statement we can avoid '{}' curly brackets if we only use print function once but its better to use curly brackets to avoid any error
+        // curly braces are used to print multiple statements
+
+
+
+        // Methods for using conditional statements
+
+        // make a program which compares 2 inputs a and  from user whether both are equal or a is greater than b or b is greater than a
+        System.out.println("Enter value of a:-");
+        int a = sc.nextInt();
+        System.out.println("Enter value of b-");
+        int b = sc.nextInt();
+
+//        Method-1       [ The wrong approach ]
+        if (a==b){
+            System.out.println("Both Numbers Are equal");
+        }
+        if (a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+
+        // using multiple if simply means all the if conditions will be checked by java one by one even if first condition is met
+        // for eg if a==b then it will print both no are same but after that it will also check for the second if i.e a>b and we know a=b so a>b will be false
+        // so the else statement will also get printed this is a common error if a==b then as a result you will get two statements as
+        // [ both numbers are equal and b is greater than a ]b but the second statement is false as a=b and a<b cant be true both at same times
+
+//        Method-2
+        if (a==b){
+            System.out.println("Both Numbers Are equal");
         }
         else {
-            System.out.println("Odd Number");
+            if (a > b) {
+                System.out.println("a is greater than b");
+            } else {
+                System.out.println("b is greater than a");
+            }
         }
+        // here first a==b will be checked if a!=b or a==b is false then else condition will be checked in which first a>b will be checked if false then a<b the final else will be checked
 
+//        Method-3         [ use of else if ]
+        if (a==b) {
+            System.out.println(" a is equal to b");
+        }
+        else if (a>b) {
+            System.out.println(" a is greater than b");
+        }
+        else {
+            System.out.println("b is greater than a");
+        }
+    // More clean version
+        if (a==b)
+            System.out.println(" a is equal to b");
+        else if (a>b)
+            System.out.println(" a is greater than b");
+        else
+            System.out.println("b is greater than a");
 
+        // here first a==b will be checked if false the else if condition will be checked i.e a>b if false a<b will be checked
+        // but agr bich mai koi condition true hojae toh java will stop checking further below written statements
 
     }
 }
